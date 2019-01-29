27d291b9ea186bdcf2eb23369cc8f8026bd1281f
Tirlipinpin <hgaudeaux@hotmail.fr>
2019-01-29 16:51:53 +0100

    :lipstick: added a box-shadow on the navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -30,22 +30,20 @@ export class Navbar extends Component<NavbarProps, {}> {
         const { url } = this.props.match;
 
         return (
-            <div className="navbar-container">
-                <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
-                    <img src={logo} className="logo" onClick={() => history.push(url)} />
-                    <Menu
-                        theme="dark"
-                        mode="horizontal"
-                        defaultSelectedKeys={['1']}
-                        style={{ lineHeight: '64px' }}
-                        className="menu-items-container"
-                    >
-                        <Menu.Item key="1"><Link to={url}>Homepage</Link></Menu.Item>
-                        <Menu.Item key="2"><Link to={`${url}/profile`}>Profile</Link></Menu.Item>
-                        <Menu.Item key="3" className="logout-button" onClick={this.logout}>Logout</Menu.Item>
-                    </Menu>
-                </Header>
-            </div>
+            <Header className="navbar-container">
+                <img src={logo} className="logo" onClick={() => history.push(url)} />
+                <Menu
+                    theme="dark"
+                    mode="horizontal"
+                    defaultSelectedKeys={['1']}
+                    style={{ lineHeight: '64px' }}
+                    className="menu-items-container"
+                >
+                    <Menu.Item key="1"><Link to={url}>Homepage</Link></Menu.Item>
+                    <Menu.Item key="2"><Link to={`${url}/profile`}>Profile</Link></Menu.Item>
+                    <Menu.Item key="3" className="logout-button" onClick={this.logout}>Logout</Menu.Item>
+                </Menu>
+            </Header>
         );
     }
 };
