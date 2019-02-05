f9b7d65b3579bf7340026f9b8e2aebfcdb8db484
Tirlipinpin <hgaudeaux@hotmail.fr>
2019-02-05 17:12:35 +0100

    :lipstick: imagined a new version of the footer

--- src/App.tsx
+++ src/App.tsx
@@ -61,7 +61,11 @@ export class App extends Component<AppProps, {}>{
                             <Route exact path={match.url} render={() => this.lazyRender(Homepage)} />
                             <Route path={`${match.path}/profile`} render={() => this.lazyRender(Profile)} />
                         </Layout.Content>
-                        <Layout.Footer style={{ textAlign: 'center' }}>
+                        <Layout.Footer style={{
+                            textAlign: 'center',
+                            background: '#002766',
+                            color: 'rgba(255, 255, 255, 0.65)'
+                        }}>
                             Lecourt ©2019 Created with <Icon type="heart" /> by the best developpers ever
                         </Layout.Footer>
                     </div>
