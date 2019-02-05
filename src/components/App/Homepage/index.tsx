b78827054d5b303d0cba9a08ee227a041fa3832d
Tirlipinpin <hgaudeaux@hotmail.fr>
2019-02-05 17:14:00 +0100

    :alembic: added  a carousel on homepage

--- src/components/App/Homepage/index.tsx
+++ src/components/App/Homepage/index.tsx
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+<<<<<<< HEAD
+=======
+import { Carousel, Layout } from 'antd';
+
+import './index.css';
+>>>>>>> fc0c7bf... :alembic: added  a carousel on homepage
 
 export class Homepage extends Component<{}, {}> {
     render() {
