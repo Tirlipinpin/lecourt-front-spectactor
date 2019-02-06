c639f60735c212c4dcf7e071d465762fda171f69
Tirlipinpin <hgaudeaux@hotmail.fr>
2019-02-06 11:38:38 +0100

    :ambulance: removed leaked feature not done

--- src/components/Homepage/index.tsx
+++ src/components/Homepage/index.tsx
@@ -1,27 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Layout, Carousel } from 'antd';
 
 export class Homepage extends Component<{}, {}> {
     render() {
         return (
-            <Layout className="homepage-page-container">
-                <Carousel
-                    autoplay
-                    dots
-                    className="homepage-carousel"
-                    autoplaySpeed={5000}
-                    speed={2000}
-                    arrows
-                    lazyLoad
-                    draggable={false}
-                >
-                    <img src="http://www.champselyseesfilmfestival.com/2018/wp-content/uploads/sites/11/2018/04/caro2.jpg" />
-                    <img src="https://wx3.sinaimg.cn/large/0078HDDZly1fryg0w2z2vj31hc0u0jus.jpg" />
-                    <img src="http://www.zippyframes.com/images/stories/italy/inanimate_lucia_bulgheroni.jpg" />
-                    <img src="https://i.ytimg.com/vi/qeAjs_9XLbk/maxresdefault.jpg" />
-                </Carousel>
-            </Layout>
+            <div>This is the Homepage</div>
         );
     }
 };
