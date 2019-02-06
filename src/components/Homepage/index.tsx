faba0e3695f6de63526507bf69309bec40bce8b5
Tirlipinpin <hgaudeaux@hotmail.fr>
2019-02-06 11:23:06 +0100

    :construction: possible next homepage

--- src/components/Homepage/index.tsx
+++ src/components/Homepage/index.tsx
@@ -1,28 +1,29 @@
-import React, { Component, Dispatch } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Layout, Carousel } from 'antd';
 
-interface homepageStore {
-    page: string,
-}
-
-interface HomepageProps {
-    homepage: homepageStore,
-};
-
-export class Homepage extends Component<HomepageProps, {}> {
+export class Homepage extends Component<{}, {}> {
     render() {
-        const { homepage } = this.props;
-
         return (
-            <div>
-                This is the { homepage.page }
-            </div>
+            <Layout className="homepage-page-container">
+                <Carousel
+                    autoplay
+                    dots
+                    className="homepage-carousel"
+                    autoplaySpeed={5000}
+                    speed={2000}
+                    arrows
+                    lazyLoad
+                    draggable={false}
+                >
+                    <img src="http://www.champselyseesfilmfestival.com/2018/wp-content/uploads/sites/11/2018/04/caro2.jpg" />
+                    <img src="https://wx3.sinaimg.cn/large/0078HDDZly1fryg0w2z2vj31hc0u0jus.jpg" />
+                    <img src="http://www.zippyframes.com/images/stories/italy/inanimate_lucia_bulgheroni.jpg" />
+                    <img src="https://i.ytimg.com/vi/qeAjs_9XLbk/maxresdefault.jpg" />
+                </Carousel>
+            </Layout>
         );
     }
 };
 
-function mapDispatchToProps({ homepage }: any) {
-    return { homepage };
-}
-
-export default connect(mapDispatchToProps)(Homepage);
+export default connect()(Homepage);
