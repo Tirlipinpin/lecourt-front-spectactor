989f62ab948126ec1726862fde814604d6ad66c9
Tirlipinpin <hgaudeaux@hotmail.fr>
2019-02-26 13:35:18 +0100

    :lipstick: moved some things on the profile page

--- src/components/App/Profile/index.tsx
+++ src/components/App/Profile/index.tsx
@@ -22,47 +22,52 @@ export class Profile extends Component<{}, {}> {
                 <Layout.Content className="profile-page-content">
                     <div className="profile-page-header">
                         <Avatar size={128} icon="user" className="user-profile-picture" />
+                        <h1>John Doe</h1>
                     </div>
-                    <Card
-                        className="card-parameters"
-                        title="Profile update"
-                        style={this.cardStyle}
-                        bodyStyle={this.cardBodyStyle}
-                        id="profile-update"
-                    ><Skeleton active paragraph={{ rows: 8 }} /></Card>
-                    <Card
-                        className="card-parameters"
-                        title="Favorites"
-                        style={this.cardStyle}
-                        bodyStyle={this.cardBodyStyle}
-                        id="favorites"
-                    ><Skeleton active paragraph={{ rows: 3 }} /></Card>
-                    <Card
-                        className="card-parameters"
-                        title="Global preferences"
-                        style={this.cardStyle}
-                        bodyStyle={this.cardBodyStyle}
-                        id="global-preferences"
-                    ><Skeleton active paragraph={{ rows: 8 }} /></Card>
-                    <Card
-                        className="card-parameters"
-                        title="Privacy settings"
-                        style={this.cardStyle}
-                        bodyStyle={this.cardBodyStyle}
-                        id="privacy-settings"
-                    ><Skeleton active paragraph={{ rows: 5 }} /></Card>
+                    <Layout>
+                        <MediaQuery minDeviceWidth={720}>
+                            <Layout.Sider className="anchor-menu">
+                                <Anchor className="anchor" offsetTop={64}>
+                                    <h2 className="anchor-title">Go to</h2>
+                                    <Anchor.Link href="#profile-update" title="Profile update" />
+                                    <Anchor.Link href="#favorites" title="Favorites" />
+                                    <Anchor.Link href="#global-preferences" title="Global preferences" />
+                                    <Anchor.Link href="#privacy-settings" title="Privacy settings" />
+                                </Anchor>
+                            </Layout.Sider>
+                        </MediaQuery>
+                        <Layout.Content className="cards-parameters-container">
+                            <Card
+                                className="card-parameters"
+                                title="Profile update"
+                                style={this.cardStyle}
+                                bodyStyle={this.cardBodyStyle}
+                                id="profile-update"
+                            ><Skeleton active paragraph={{ rows: 8 }} /></Card>
+                            <Card
+                                className="card-parameters"
+                                title="Favorites"
+                                style={this.cardStyle}
+                                bodyStyle={this.cardBodyStyle}
+                                id="favorites"
+                            ><Skeleton active paragraph={{ rows: 3 }} /></Card>
+                            <Card
+                                className="card-parameters"
+                                title="Global preferences"
+                                style={this.cardStyle}
+                                bodyStyle={this.cardBodyStyle}
+                                id="global-preferences"
+                            ><Skeleton active paragraph={{ rows: 8 }} /></Card>
+                            <Card
+                                className="card-parameters"
+                                title="Privacy settings"
+                                style={this.cardStyle}
+                                bodyStyle={this.cardBodyStyle}
+                                id="privacy-settings"
+                            ><Skeleton active paragraph={{ rows: 5 }} /></Card>
+                        </Layout.Content>
+                    </Layout>
                 </Layout.Content>
-                <MediaQuery minDeviceWidth={720}>
-                    <Layout.Sider className="anchor-menu">
-                        <Anchor className="anchor" offsetTop={128}>
-                            <h2 className="anchor-title">Go to</h2>
-                            <Anchor.Link href="#profile-update" title="Profile update" />
-                            <Anchor.Link href="#favorites" title="Favorites" />
-                            <Anchor.Link href="#global-preferences" title="Global preferences" />
-                            <Anchor.Link href="#privacy-settings" title="Privacy settings" />
-                        </Anchor>
-                    </Layout.Sider>
-                </MediaQuery>
             </Layout>
         );
     }
