9725b8321c76cbae346d94ac9515b8c9d1212a7b
Tirlipinpin <hgaudeaux@hotmail.fr>
2019-03-12 11:57:06 +0100

    :white_check_mark: best assertion on homepage behavior

--- src/components/App/Homepage/index.test.tsx
+++ src/components/App/Homepage/index.test.tsx
@@ -3,18 +3,44 @@ import Adapter from 'enzyme-adapter-react-16';
 import Enzyme, { shallow } from 'enzyme';
 import { createMemoryHistory } from 'history'
 
+import { HomepageStore } from '../../../reducers/homepage';
 import { Homepage } from '.';
 
 Enzyme.configure({ adapter: new Adapter() })
 
 describe('the Homepage component', () => {
     let wrapper;
+    const dispatch = jest.fn();
 
     beforeEach(() => {
-        wrapper = shallow(<Homepage history={createMemoryHistory()} />);
+        const homepage: HomepageStore = {
+            movies: [],
+            latestMovies: [],
+        }
+        wrapper = shallow(
+            <Homepage
+                homepage={homepage}
+                dispatch={dispatch}
+                history={createMemoryHistory()}
+            />
+        );
     });
 
     it('should render correctly', () => {
         expect(wrapper.length).toEqual(1);
+    });
+
+    it('should dispatch FETCH_MOVIES twice with simple and latest requests', () => {
+        expect(dispatch).toHaveBeenNthCalledWith(1, {
+            type: 'FETCH_MOVIES',
+            payload: {},
+        });
+
+        expect(dispatch).toHaveBeenNthCalledWith(2, {
+            type: 'FETCH_MOVIES',
+            payload: {
+                latest: true,
+            },
+        });
     });
 });
