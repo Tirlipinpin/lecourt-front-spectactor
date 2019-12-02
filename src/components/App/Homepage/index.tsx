3b4b797d36e00be2c6e6e2633a69df98e7929f11
tirlipinpin <hgaudeaux@hotmail.fr>
2019-12-02 11:22:56 +0100

    :lipstick: temporary homepage

--- src/components/App/Homepage/index.tsx
+++ src/components/App/Homepage/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, Fragment } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Layout, Typography } from 'antd';
 import { RouteComponentProps } from 'react-router';
@@ -6,22 +6,7 @@ import { Trans, useTranslation } from 'react-i18next';
 
 import MoviesGallery from '../../shared/MoviesGallery';
 import { fetchLatestMovies } from './actions';
-import { RenderPageStructures } from '../services';
 import styles from './index.module.scss';
-import { HomepageStore } from '../../../reducers/homepage';
-
-const content = (homepage: HomepageStore, history: RouteComponentProps['history']) => (
-    <Fragment>
-      <Layout className={styles.moviesCarousel}>
-          <Typography.Title level={2}><Trans i18nKey="OUR_SELECTION" /></Typography.Title>
-          <MoviesGallery loading={homepage.loadingLatestMovies} movies={homepage.latestMovies} history={history} />
-      </Layout>
-      <Layout className={styles.moviesCarousel}>
-          <Typography.Title level={2}><Trans i18nKey="LATEST_SHORTS" /></Typography.Title>
-          <MoviesGallery loading={homepage.loadingLatestMovies} movies={homepage.latestMovies} history={history} />
-      </Layout>
-    </Fragment>
-);
 
 export interface IHomepageProps extends RouteComponentProps {}
 
@@ -35,18 +20,21 @@ export const Homepage = ({ history }: IHomepageProps) => {
     }, []);
 
   return (
-    <RenderPageStructures
-      title={t('WELCOME_TO_LECOURT')}
-      Header={(
-        <React.Fragment>
-            <Typography.Paragraph>
-                {t('LOOK_AT_OUR_SHORTS')}
-            </Typography.Paragraph>
-        </React.Fragment>
-    )}
-    Child={content(homepage, history)}
-    />
-  );   
+    <div className={styles.pageContainer}>
+      <Layout className={styles.moviesCarousel}>
+          <Typography.Title level={3}>{t('OUR_SELECTION')}</Typography.Title>
+          <MoviesGallery
+            loading={homepage.loadingLatestMovies}
+            movies={homepage.latestMovies}
+            history={history}
+          />
+      </Layout>
+      <Layout className={styles.moviesCarousel}>
+          <Typography.Title level={3}><Trans i18nKey="LATEST_SHORTS" /></Typography.Title>
+          <MoviesGallery loading={homepage.loadingLatestMovies} movies={homepage.latestMovies} history={history} />
+      </Layout>
+    </div>
+  );
 };
 
 export default Homepage;
