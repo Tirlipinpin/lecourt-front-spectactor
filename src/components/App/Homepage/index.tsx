77dfaa37c5801cbf955a23cc3937821019c59299
Tirlipinpin <hgaudeaux@hotmail.fr>
2019-12-03 11:11:05 +0100

    :lipstick: border-bottom on homepage titles

--- src/components/App/Homepage/index.tsx
+++ src/components/App/Homepage/index.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Layout, Typography } from 'antd';
 import { RouteComponentProps } from 'react-router';
-import { Trans, useTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 
 import MoviesGallery from '../../shared/MoviesGallery';
 import { fetchLatestMovies } from './actions';
@@ -22,7 +22,11 @@ export const Homepage = ({ history }: IHomepageProps) => {
   return (
     <div className={styles.pageContainer}>
       <Layout className={styles.moviesCarousel}>
-          <Typography.Title level={3}>{t('OUR_SELECTION')}</Typography.Title>
+          <Typography.Title
+            level={3}
+            children={t('OUR_SELECTION')}
+            className={styles.sectionTitle}
+          />
           <MoviesGallery
             loading={homepage.loadingLatestMovies}
             movies={homepage.latestMovies}
@@ -30,7 +34,11 @@ export const Homepage = ({ history }: IHomepageProps) => {
           />
       </Layout>
       <Layout className={styles.moviesCarousel}>
-          <Typography.Title level={3}><Trans i18nKey="LATEST_SHORTS" /></Typography.Title>
+      <Typography.Title
+            level={3}
+            children={t('LATEST_SHORTS')}
+            className={styles.sectionTitle}
+          />
           <MoviesGallery loading={homepage.loadingLatestMovies} movies={homepage.latestMovies} history={history} />
       </Layout>
     </div>
